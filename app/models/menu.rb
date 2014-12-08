5fb28dad52ec3efc79436612328f3d2f50146e3f
Uday Kadaboina <uday88k@gmail.com>
2014-12-07 22:42:21 -0500

    made db relations

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,2 +1,4 @@
 class Menu < ActiveRecord::Base
+  belongs_to :restaurant
+  has_many :items
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,12 @@
 class Restaurant < ActiveRecord::Base
+  has_many :menus
+  has_one :location
+
+  # It returns the articles whose titles contain one or more words that form the query
+  def self.search(query)
+    # where(:title, query) -> This would return an exact match of the query
+    # Restaurant.where("location.zip like ?", "%#{query}%") 
+    binding.pry
+  end
+
 end
